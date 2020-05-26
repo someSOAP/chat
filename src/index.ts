ed5e3d4391616dff17e9f67acb4a1755fc81c54e
someSOAP <andrey.meg@mail.ru>
2020-05-26 22:35:02 +0300

    Testy entities and controllers

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,34 @@
-import 'dotenv/config';
 import "reflect-metadata";
-import cors from "cors";
-import express from 'express';
+import 'dotenv/config';
+// import cors from "cors";
+import { createConnection } from "typeorm";
+import { Request, Response } from "express";
+import express from "express";
+import bodyParser from "body-parser";
+import { AppRoutes } from "./routes";
 
+// create connection with database
+// note that it's not active database connection
+// TypeORM creates connection pools and uses them for your requests
+createConnection().then(async connection => {
 
-const app = express();
-app.use(cors());
+    // create express app
+    const app = express();
+    // app.use(cors());
+    app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+    // register all application routes
+    AppRoutes.forEach(route => {
+        app[route.method](route.path, (request: Request, response: Response, next: Function) => {
+            route.action(request, response)
+                .then(() => next)
+                .catch(err => next(err));
+        });
+    });
 
+    // run app
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT);
+    console.log(`Express application is up and running on port ${PORT}`);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-    console.log(`Applications is running on port ${PORT}!`);
-});
+}).catch(error => console.log("TypeORM connection error: ", error));
